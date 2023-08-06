f0c09ed7e00c670dbd735afcb5ee729651ab5980
6SYNAPESE9 <shayaanelahi@gmail.com>
2023-08-06 11:27:56 +0200

    Railway app

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-13%o=ruhq!3-a$if#%0!3h8e+yxd^-2i9u@9@j982fannftqd&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
